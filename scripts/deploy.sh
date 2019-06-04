8be3696417641b29e1da9313404aba0ffe5204dd
Nathaniel Stevens <nstevens@pillartechnology.com>
2019-06-04 11:58:14 -0400

    SMRT-1284 Add travis file and change presto and metastore testing images to use locally built base images
    co-authored-by: Cameron Marsh <cmarsh@pillartechnology.com>

---
+++ scripts/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+IMAGE=$1
+RELEASE_TYPE=$2
+echo "Logging into Dockerhub ..."
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+
+echo "Determining image tag for ${TRAVIS_BRANCH} build ..."
+
+if [[ $RELEASE_TYPE == "release" ]]; then
+  export TAGGED_IMAGE="smartcitiesdata/${IMAGE}:${TRAVIS_BRANCH}"
+elif [[ $RELEASE_TYPE == "master" ]]; then
+  export TAGGED_IMAGE="smartcitiesdata/${IMAGE}:development"
+else
+    echo "Branch should not be pushed to Dockerhub"
+    exit 0
+fi
+
+echo "Pushing to Dockerhub with tag ${TAGGED_IMAGE} ..."
+
+docker tag kdp:build "${TAGGED_IMAGE}"
+docker push "${TAGGED_IMAGE}"
